e79c3229ec7cb05a84a8a6831798cc69dcb4cc6d
William Hilburn <123123775+whilburncsumb@users.noreply.github.com>
2024-04-18 23:26:36 -0700

    Animations for the alarm clock

--- Assets/Scripts/CountdownPie.cs
+++ Assets/Scripts/CountdownPie.cs
@@ -9,9 +9,13 @@ public class PieTimer : MonoBehaviour
     private bool timeUp = true;
     public MainGameController gameController;
     public GameObject ClockHand;
+    private Animator anim;
+    public Color[] colors;
     private void Start()
     {
         gameController = FindObjectOfType<MainGameController>();
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
     }
     
     public void StartTimer()
@@ -19,6 +23,7 @@ public class PieTimer : MonoBehaviour
         maxTime = MainGameController.timeRemaining;
         currentTime = maxTime;
         timeUp = false;
+        anim.Play("ClockAnimation1");
     }
 
     void Update()
@@ -36,12 +41,23 @@ public class PieTimer : MonoBehaviour
             // Handle timer expiration
             Debug.Log("Time up!");
             timeUp = true;
+            anim.Play("ClockAnimation2");
             // gameController.MinigameDone(false);
         }
 
         float fillAmount = currentTime / maxTime;
+        float ratioDone = 1 - fillAmount;
         pieImage.fillAmount = fillAmount;
         Quaternion rotation = Quaternion.Euler(0f, 0f, (1 - fillAmount) * 360f);
         ClockHand.transform.rotation = rotation;
+        if (fillAmount > 0)
+        {
+            anim.speed = 1f / fillAmount;
+            pieImage.color = colors[(int)Mathf.Floor(ratioDone*colors.Length)];
+        }
+        else
+        {
+            anim.speed = 10f;
+        }
     }
 }
