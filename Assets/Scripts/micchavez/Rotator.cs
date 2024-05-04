46b18916b3a0be773e8f7876731b96ab0f61caa7
chavezMac <chavom68@gmail.com>
2024-05-03 19:09:46 -0700

    Audio and improvements to minigames 1 and 2 per William's comments

--- Assets/Scripts/micchavez/Rotator.cs
+++ Assets/Scripts/micchavez/Rotator.cs
@@ -6,8 +6,10 @@ public class Rotator : MonoBehaviour
 {
     // Update is called once per frame
     void Update(){
+        //Rotate the object around its local Y axis
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
 
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        //transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
         
     }
 }
