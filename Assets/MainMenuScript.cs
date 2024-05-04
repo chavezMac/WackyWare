70e53178a023cae6b944a6bd957aaa20b704bb2c
William Hilburn <123123775+whilburncsumb@users.noreply.github.com>
2024-05-04 10:10:21 -0700

    Started work on main menu

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button howtoplayButton;
+    public GameObject titleText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
