b21d69df889e07c1cc40bc549f4b68008329b87f
Jesus Ramos <jesusri902@gmail.com>
2024-05-07 11:10:22 -0700

    Update
    Hey I got rid of Unwanted Packages from my files, I organized my Scripts, I added music and background to minigame 3. Still working on Debugging jump, and adding animation

---
+++ Assets/Scripts/JesusScripts/PlayerMoves.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    public Transform cam;
+    public float speed = 5f;
+    public float jumpForce = 8f;
+    public float jumpSpeed;
+    public Rigidbody rb;
+    public LayerMask groundMask;
+    float distanceToGround = 0.05f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    private float ySpeed;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Movement
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalMovement, 0f, verticalMovement).normalized;
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        UnityEngine.Debug.DrawRay(transform.position, Vector3.down * (distanceToGround + 0.1f), Color.green);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.Translate(moveDir.normalized * speed * Time.deltaTime, Space.World);
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            UnityEngine.Debug.Log("spacebar is pressed");
+            ySpeed = jumpSpeed;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        
+        return Physics.Raycast(transform.position, Vector3.down, distanceToGround + 0.1f, groundMask);
+    }
+
+}
